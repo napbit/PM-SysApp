5e433fc9f25ab5921ccb79bcefaef4d11fe08b7b
Afian <nap.tyo@gmail.com>
2018-04-22 18:34:52 +0700

    Added backing for registration (albeit empty).

---
+++ PMSysAppEJB/ejbModule/com/binus/pmsys/backing/RegistrationBacking.java
@@ -0,0 +1,13 @@
+package com.binus.pmsys.backing;
+
+import javax.faces.bean.ViewScoped;
+import javax.inject.Named;
+
+@Named
+@ViewScoped
+public class RegistrationBacking extends BasicBacking {
+	private static final long serialVersionUID = 8664948727348463034L;
+
+	public RegistrationBacking() { }
+
+}
