1ed9c2574650a732a769610fa247451629078f8b
Afian <nap.tyo@gmail.com>
2018-04-25 19:20:31 +0700

    Added Setter + Getters

--- PMSysAppEJB/ejbModule/com/binus/pmsys/backing/RegistrationBacking.java
+++ PMSysAppEJB/ejbModule/com/binus/pmsys/backing/RegistrationBacking.java
@@ -3,11 +3,23 @@ package com.binus.pmsys.backing;
 import javax.faces.bean.ViewScoped;
 import javax.inject.Named;
 
+import com.binus.pmsys.entity.Patient;
+
 @Named
 @ViewScoped
 public class RegistrationBacking extends BasicBacking {
 	private static final long serialVersionUID = 8664948727348463034L;
-
+	
+	private Patient patient;
+	
 	public RegistrationBacking() { }
+
+	public Patient getPatient() {
+		return patient;
+	}
+
+	public void setPatient(Patient patient) {
+		this.patient = patient;
+	}
 
 }
